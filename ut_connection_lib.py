04a14cd5f0eadf6be51b48e8170aadb58b3287f9
ridhomakbar <ridhomakbar@gmail.com>
2019-04-25 07:46:14 +0700

    Add files via upload

---
+++ ut_connection_lib.py
@@ -0,0 +1,50 @@
+import pyodbc
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+
+class Connection:
+    def __init__(self, 
+                 dbname="xxx", 
+                 dbhost="xx.x.xx.xxx", 
+                 dbport="xxxx",
+                 dbuser="xxxx",
+                 dbpass="xxxx"):
+        self.dbname = dbname
+        self.dbhost = dbhost
+        self.dbport = dbport
+        self.dbuser = dbuser
+        self.dbpass = dbpass
+        self.conn, self.cursor = self.create_connection_to_netezza()
+       
+    def create_connection_to_netezza(self):
+        connection_string = "Driver={};servername={}"\
+                              ";port={};database={}"\
+                              ";username={};password={};".format(
+                                  "{NetezzaSQL}",self.dbhost, self.dbport, 
+                                  self.dbname, self.dbuser, self.dbpass)
+        conn_engine = pyodbc.connect(connection_string)
+        db_cursor = conn_engine.cursor()
+        return (conn_engine, db_cursor)
+    
+class Query:
+    def __init__(self, query_string=None, source_file=None):
+        self.source_file = source_file
+        if source_file is not None:
+            self.query = self.read_file()
+        else:
+            self.query = query_string
+    
+    def execute(self, connection):
+        if self.query is not None and len(self.query)>0:
+            return pd.read_sql(self.query, connection.conn)
+        else:
+            print("Error: Query string cannot be empty")
+    
+    def read_file(self):
+        f = open(self.source_file, 'r')
+        query_string = ""
+        for row in f:
+            query_string += row.replace('\n',' ')
+        f.close()
+        return query_string
